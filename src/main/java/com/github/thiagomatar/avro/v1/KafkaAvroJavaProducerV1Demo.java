78e20efea65445d48254ca17bcc2a27def335efc
thiago <thiago.matar@msn.com>
2019-12-17 16:52:21 -0300

    Creating Kafka's Producers and Consumers with Avro

---
+++ src/main/java/com/github/thiagomatar/avro/v1/KafkaAvroJavaProducerV1Demo.java
@@ -0,0 +1,61 @@
+package com.github.thiagomatar.avro.v1;
+
+import com.example.CustomerV1;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class KafkaAvroJavaProducerV1Demo {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        // normal producer
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("acks", "all");
+        properties.setProperty("retries", "10");
+        // avro part
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", KafkaAvroSerializer.class.getName());
+        properties.setProperty("schema.registry.url", "http://127.0.0.1:8081");
+
+        Producer<String, CustomerV1> producer = new KafkaProducer<String, CustomerV1>(properties);
+
+        String topic = "customer-avro1";
+
+        // copied from avro examples
+        CustomerV1 customer = CustomerV1.newBuilder()
+                .setAge(34)
+                .setAutomatedEmail(false)
+                .setFirstName("John")
+                .setLastName("Doe")
+                .setHeight(178f)
+                .setWeight(75f)
+                .build();
+
+        ProducerRecord<String, CustomerV1> producerRecord = new ProducerRecord<String, CustomerV1>(
+                topic, customer
+        );
+
+        System.out.println(customer);
+        producer.send(producerRecord, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                if (exception == null) {
+                    System.out.println(metadata);
+                } else {
+                    exception.printStackTrace();
+                }
+            }
+        });
+
+        producer.flush();
+        producer.close();
+
+    }
+}
